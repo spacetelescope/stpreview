bc10256bb7bf56715a7822615f54771e2f942efb
zacharyburnett <zburnett@stsci.edu>
2023-08-17 13:49:54 -0400

    initial commit

---
+++ src/downsample/__main__.py
@@ -0,0 +1,23 @@
+import typer
+from pathlib import Path
+
+
+app = typer.Typer()
+
+
+@app.command()
+def to(resolution: tuple[int, int], filename: Path):
+    print(f'{resolution} {filename}')
+
+
+@app.command()
+def by(factor: float, filename: Path):
+    print(f'{factor} {filename}')
+
+
+def command():
+    app()
+
+
+if __name__ == "__main__":
+    command()
