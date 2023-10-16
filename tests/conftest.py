e7b92cfb0d505ff2e062840276accab9d26afbdf
zacharyburnett <zburnett@stsci.edu>
2023-10-16 16:52:18 -0400

    add shareddata tests

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+# content of conftest.py
+
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--shareddata",
+        action="store_true",
+        default=False,
+        help="run tests on shared data",
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--shareddata"):
+        return
+    skip_shareddata = pytest.mark.skip(reason="need --shareddata option to run")
+    for item in items:
+        if "shareddata" in item.keywords:
+            item.add_marker(skip_shareddata)
---
+++ tests/test_roman_data.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+
+import pytest
+
+from stpreview.downsample import downsample_asdf
+
+DATA_DIRECTORY = Path(__file__).parent / "data"
+SHARED_DATA_DIRECTORY = Path("/grp/roman/TEST_DATA/23Q4_B11/aligntest")
+
+
+def level1_science_raw(data_directory) -> Path:
+    filename = data_directory / "level1_science_raw.asdf"
+    if not filename.exists():
+        from roman_datamodels.maker_utils import mk_level1_science_raw
+
+        mk_level1_science_raw(filepath=filename)
+
+    return filename
+
+
+def level2_image(data_directory) -> Path:
+    filename = data_directory / "level2_image.asdf"
+    if not filename.exists():
+        from roman_datamodels.maker_utils import mk_level2_image
+
+        mk_level2_image(filepath=filename)
+
+    return filename
+
+
+def level3_mosaic(data_directory) -> Path:
+    filename = data_directory / "level3_mosaic.asdf"
+    if not filename.exists():
+        from roman_datamodels.maker_utils import mk_level3_mosaic
+
+        mk_level3_mosaic(filepath=filename)
+
+    return filename
+
+
+@pytest.mark.parametrize(
+    "filename,shape",
+    [
+        (level1_science_raw(DATA_DIRECTORY), 3),
+        (level2_image(DATA_DIRECTORY), 2),
+        (level3_mosaic(DATA_DIRECTORY), 2),
+    ],
+)
+def test_dummy_data(filename, shape):
+    result = downsample_asdf(filename, by=2)
+
+    assert len(result.shape) == shape
+
+
+@pytest.mark.shareddata
+@pytest.mark.skipif(
+    not SHARED_DATA_DIRECTORY.exists(), reason="can't reach shared data directory"
+)
+@pytest.mark.parametrize(
+    "filename,shape",
+    [
+        (filename, 3 if "uncal" in str(filename) else 2)
+        for filename in SHARED_DATA_DIRECTORY.iterdir()
+        if filename.suffix.lower() == ".asdf"
+    ],
+)
+def test_sample_data(filename, shape):
+    result = downsample_asdf(filename, by=2)
+
+    assert len(result.shape) == shape
