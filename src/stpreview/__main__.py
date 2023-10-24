35c043a6401fa792944c12bd7e82dce481c0d3bc
zacharyburnett <zburnett@stsci.edu>
2023-10-24 08:47:28 -0400

    save to image file

--- src/stpreview/__main__.py
+++ src/stpreview/__main__.py
@@ -1,11 +1,19 @@
 from pathlib import Path
 
-import asdf
 import typer
+from PIL import Image
 
 from stpreview.downsample import downsample_asdf_by, downsample_asdf_to
 
 app = typer.Typer()
+
+
+@app.command()
+def downsample_by(factor: tuple[int, int], input: Path, output: Path):
+    data = downsample_asdf_by(input=input, by=factor)
+
+    image = Image.fromarray(data)
+    image.save(output)
 
 
 @app.command()
@@ -14,16 +22,8 @@ def downsample_to(
 ):
     data = downsample_asdf_to(resolution, input, observatory)
 
-    with asdf.open(output, mode="rw") as file:
-        file.write(data)
-
-
-@app.command()
-def downsample_by(factor: tuple[int, int], input: Path, output: Path):
-    data = downsample_asdf_by(input=input, by=factor)
-
-    with asdf.open(output, mode="rw") as file:
-        file.write(data)
+    image = Image.fromarray(data)
+    image.save(output)
 
 
 def command():
